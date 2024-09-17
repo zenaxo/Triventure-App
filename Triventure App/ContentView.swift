08ebcf59b5ffd9cb77cce0bd494f1bdaca25013e
zenaxo <sjolander48@live.se>
2024-09-17 09:46:14 +0200

    Initial Commit

---
+++ Triventure App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Triventure App
+//
+//  Created by Hannes Sjölander on 2024-09-17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Triventure App/Triventure_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Triventure_AppApp.swift
+//  Triventure App
+//
+//  Created by Hannes Sjölander on 2024-09-17.
+//
+
+import SwiftUI
+
+@main
+struct Triventure_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
